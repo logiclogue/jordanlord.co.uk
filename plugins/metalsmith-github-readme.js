ff60c49398d046ccd8b0f9f760b584d92ab05bd0
logiclogue <jordanpaullord@hotmail.co.uk>
2017-05-08 20:12:20 +0100

    Updates github readme script to remove author seciont

--- plugins/metalsmith-github-readme.js
+++ plugins/metalsmith-github-readme.js
@@ -33,6 +33,8 @@ function plugin() {
         sectionsObj.sections.map(function (section) {
             if (section.level === 1) {
                 section.string = section.string.replace(section.heading, '');
+            } else if (section.title.toLowerCase() === 'author') {
+                section.string = '';
             }
         });
 
